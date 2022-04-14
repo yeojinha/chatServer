a002289624657bfbe92557e38140ec73aa8cd9c0
yeojinha <hayeojin4966@gmail.com>
2022-04-14 23:17:42 +0900

    websocket 주석

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const WebSocket = require('ws');
-const port = process.env.PORT || 3000
+const WebSocket = require("ws");
+const port = process.env.PORT || 3000;
 
 app.use(
   cors({
@@ -12,41 +12,40 @@ app.use(
 );
 
 const server = app.listen(port, () => {
-  console.log(port, 'waiting unitil connects');
+  console.log(port, "waiting unitil connects");
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-const wss = new WebSocket.Server({ server, path: '/ws' });
+// const wss = new WebSocket.Server({ server, path: "/ws" });
 
-wss.on('connection', (ws, req) => {
-  // connection
-  console.log('새로운 클라이언트 접속');
-  ws.on('message', (message) => {
-    // receiving message
-    const json = JSON.parse(message.toString());
-    json.time = Date.now()
-    message = JSON.stringify(json)
-    console.log(message.toString());
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
+// wss.on("connection", (ws, req) => {
+//   // connection
+//   console.log("새로운 클라이언트 접속");
+//   ws.on("message", (message) => {
+//     // receiving message
+//     const json = JSON.parse(message.toString());
+//     json.time = Date.now();
+//     message = JSON.stringify(json);
+//     console.log(message.toString());
+//     wss.clients.forEach((client) => {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(message.toString());
+//       }
+//     });
 
-      // Runs when client disconnects
-  wss.on('disconnect', () => {
-  });
-  });
-  ws.on('error', (err) => {
-    // error 
-    console.error(err);
-  });
-  ws.on('close', () => {
-    // close
-    console.log('Client close');
-    clearInterval(ws.interval);
-  });
-});
+//     // Runs when client disconnects
+//     wss.on("disconnect", () => {});
+//   });
+//   ws.on("error", (err) => {
+//     // error
+//     console.error(err);
+//   });
+//   ws.on("close", () => {
+//     // close
+//     console.log("Client close");
+//     clearInterval(ws.interval);
+//   });
+// });
