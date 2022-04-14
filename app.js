20d3696b569f2015fb02579fdd6c89edbe8928c0
yeojinha <hayeojin4966@gmail.com>
2022-04-14 23:30:09 +0900

    최소한코드 빼고 다 주석처리

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const cors = require("cors");
+// const cors = require("cors");
 const express = require("express");
 const app = express();
-const WebSocket = require("ws");
+// const WebSocket = require("ws");
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//   })
+// );
 
 const server = app.listen(port, () => {
   console.log(port, "waiting unitil connects");
